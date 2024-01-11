cae1f24284041d441f54890e52c851b1e6371dc3
Tomprazie <cedxboutin@gmail.com>
2024-01-11 09:20:01 -0500

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_cACeWDXuT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5353nrfp0g7uoo0ub5qrdm7mnv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. eu-west-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
